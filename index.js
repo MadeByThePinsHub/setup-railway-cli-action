83b8b3e19c430483352096de34cfb0f6c057c549
Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>
2021-05-29 12:38:12 +0800

    chore(global): Update the action code + gitignore for builds from ncc to be included in future commits
    Signed-off-by: Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-/*
- * For now, we need to do 'import * as something from 'package'
- * because GitHub Actions doesn't like ES6 for now.
- * And as usual, we'll complie this with Vercel's NCC.
- */
 const core = require("@actions/core");
 const Toolkit = require("actions-toolkit");
 const { spawn } = require("child_process");
@@ -13,6 +8,7 @@ const path = require("path");
 const cliRepoUrl = core.getInput("repo-url");
 const cliRepoBranch = core.getInput("repo-branch");
 const installThroughNpmjs = core.getBooleanInput("npm-mode");
+const workspace = process.env.GITHUB_WORKSPACE;
 
 /**
  * Execute shell command
@@ -29,7 +25,7 @@ const exec = (cmd, args = []) =>
       stdout = data;
     });
     app.on("close", (code) => {
-      if (code !== 0 && !stdout.includes("nothing to commit")) {
+      if (code !== 0 && !stdout.includes("Successfully ran command.")) {
         err = new Error(`Invalid status code: ${code}`);
         err.code = code;
         return reject(err);
@@ -40,7 +36,7 @@ const exec = (cmd, args = []) =>
   });
 
 const installFromNpmjs = async () => {
-  core.debug("Installing Railway CLI through npmjs")
+  core.debug("Installing Railway CLI through npmjs");
   await exec("npm", ["install", "@railway/cli", "-g"]);
 };
 
@@ -52,31 +48,53 @@ const defaultInstaller = async () => {
   ]);
 };
 
+const installFromSource = async () => {
+  if (!cliRepoBranch) {
+    await exec("git", ["clone", cliRepoUrl, workspace + "/.railwayappcli"]);
+  } else {
+    await exec("git", [
+      "clone",
+      "-b",
+      cliRepoBranch,
+      cliRepoUrl,
+      workspace + "/.railwayappcli",
+    ]);
+  }
+  await exec("sh", [
+    "-c",
+    "$(curl -sSL https://gist.githubusercontent.com/AndreiJirohHaliliDev2006/54fb09207e1a1589a4caafb8510a25d7/raw/61837f8f3a07b3a2429ef2f78524c6bf2aeb63fa/railwayapp-cli-build.sh)",
+  ]);
+  const cliPath = workspace + "./railwayappcli/bin/railway";
+  core.addPath(cliPath);
+};
+
 // the main script
 const mainScript = async (tools) => {
-    if (!cliRepoBranch && cliRepoUrl != "") {
-        core.warning(
-          "Custom Railway CLI repo URL found, but branch is undefined. Falling back to Git defaults..."
-        );
-      }
-      
-      if (cliRepoUrl != "" && cliRepoBranch != "" && installThroughNpmjs == true) {
-        core.error(
-          "Installing the Railway CLI through npmjs doesn't work with build-from-source mode."
-        );
-      }
+  if (!cliRepoBranch && cliRepoUrl != "") {
+    core.warning(
+      "Custom Railway CLI repo URL found, but branch is undefined. Falling back to Git defaults..."
+    );
+  }
 
-      if (installThroughNpmjs == true) {
-          // Install through NPM
-          await installFromNpmjs();
-      } else {
-          // fall back to this
-          await defaultInstaller();
-      }
-}
+  if (cliRepoUrl != "" && cliRepoBranch != "" && installThroughNpmjs == true) {
+    core.error(
+      "Installing the Railway CLI through npmjs doesn't work with build-from-source mode."
+    );
+  }
 
-// this is where the main
+  if (installThroughNpmjs == true) {
+    // Install through NPM
+    await installFromNpmjs();
+  } else if (cliRepoUrl) {
+    await InstallFromSource();
+  } else {
+    // fall back to this
+    await defaultInstaller();
+  }
+};
+
+// this is where the we can call everything
 Toolkit.run(async (tools) => {
-   // Install dependencies using this function
-   await mainScript();
+  // Install dependencies using this function
+  await mainScript();
 });
